915079551061b117fd20c55b0249977ab2e80d85
Staci Amburgey <staci.m.amburgey@gmail.com>
2020-06-23 14:12:51 -0700

    Sending gitignore, script, and project file

---
+++ PrepData.R
@@ -0,0 +1,9 @@
+### Preparing data for SCR analysis for VIS lure vs no lure project
+### Data was collected in 2015 where biologists walked transects in CP that either had traps with mice (lure) or no traps (no lure)
+### The goal is to understand if detection probability is different between these two scenarios
+### In the case of an incipient or suppressed (low density) population, any way that can maximize detection probability of brown treesnakes can be a useful resource for surveyors
+
+rm(list = ls())
+
+
+caps <- read.csv()
