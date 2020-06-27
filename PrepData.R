3c05a01191740bdad99c1fb0c854e0bacbc37074
Staci Amburgey <staci.m.amburgey@gmail.com>
2020-06-27 08:16:24 -0700

    Model setup and JAGS code

--- PrepData.R
+++ PrepData.R
@@ -36,6 +36,7 @@ PrepDat <- function(caps,survs){
   ## T transect is TL when BHNZ is being run (23-March to 30-March) but NTL otherwise
   ## Remove two snakes found on forest edge and concrete barrier (i.e., not on transect)
   all3 <- all2[!(!is.na(all2$PITTAG) & is.na(all2$LOCATION)),]
+  ## all3 has a weird number of rows that doesn't match surveypts because sometimes more than one snake found at a point and also some snakes found on random other transects while searching (i.e., wouldn't show up as being surveyed in the survey file)
   ## Add TYPE == NTL to all with missing survey info, found in MASTER-ARCHIVAL-MARKREL-ALL file
   for(i in 1:nrow(all3)){
     if(!is.na(all3[i,6]) & is.na(all3[i,12])){
@@ -43,25 +44,50 @@ PrepDat <- function(caps,survs){
     }
   }
   
-  ## all3 has a weird number of rows that doesn't match surveypts because sometimes more than one snake found at a point and also some snakes found on random other transects while searching (i.e., wouldn't show up as being surveyed in the survey file)
+  
+  ##### Split into TL and NTL datasets to create capture and active dataframes #####
+  ntl <- subset(all3, TYPE == "NTL")
+  tl <- subset(all3, TYPE == "TL")
+  ## out of interest, quick sum of number of snakes caught on each kind of survey
+  # ct1 <- ntl %>% summarise(NTLcount = sum(!is.na(PITTAG)))
+  # sur1 <- ntl %>% summarise(NTLsurvs = sum(!is.na(Date2)))
+  # ct2 <- tl %>% summarise(TLcount = sum(!is.na(PITTAG)))
+  # sur2 <- tl %>% summarise(TLsurvs = sum(!is.na(Date2)))
+  # ratioNTL <- ct1/sur1
+  # ratioTL <- ct2/sur2  # Raw data looks slightly (very) higher rate of picking up snakes on VIS lure
   
   ##### Create Transect grid cell by Date dataframe to indicate active/inactive (walked/not walked) #####
-  act <- all3[,c("Date2","TRANID")]
-  act$Active <- 1
-  act <- dcast(act, TRANID ~ Date2)
-  act <- act %>% mutate_if(is.numeric, ~1 * (. > 0))
-  act <- act[match(siteord, act$TRANID),]
+  actNTL <- ntl[,c("Date2","TRANID")]
+  actNTL$Active <- 1
+  actNTL <- dcast(actNTL, TRANID ~ Date2)
+  actNTL <- actNTL %>% mutate_if(is.numeric, ~1 * (. > 0))  
+  ## Sort here so everything matching below
+  actNTL$test <- factor(actNTL$TRANID,levels=siteord)
+  actNTL <- actNTL[order(actNTL$test),]
+  
+  actTL <- tl[,c("Date2","TRANID")]
+  actTL$Active <- 1
+  actTL <- dcast(actTL, TRANID ~ Date2)
+  actTL <- actTL %>% mutate_if(is.numeric, ~1 * (. > 0))
+  ## Sort here so everything matching below
+  actTL$test <- factor(actTL$TRANID,levels=siteord)
+  actTL <- actTL[order(actTL$test),]
   
   
   ##### Create PITTAG by Date dataframe #####
   ## Add 0 or 1 to indicate captured snake at survey
   ## Missing 028347041 from original captures file, never seen during this period of time aside from once when not on transect (removed above)
   ## Sum of captures equals capture raw data - 2 snakes removed above not on transects
-  all3$Cap <- ifelse(is.na(all3$PITTAG), 0, 1)
-  snks <- dcast(data = subset(all3, Cap == 1), formula = PITTAG ~ Date2)
+  ntl$Cap <- ifelse(is.na(ntl$PITTAG), 0, 1)
+  NL <- ntl[,12]
+  snksNTL <- dcast(data = ntl, formula = PITTAG ~ Date2)[-95,]
+  
+  tl$Cap <- ifelse(is.na(tl$PITTAG), 0, 1)
+  L <- tl[,12]
+  snksTL <- dcast(data = tl, formula = PITTAG ~ Date2)[-63,]
   
   
-  prepdat <- list(act = act, snks = snks)
+  prepdat <- list(actNTL = actNTL, snksNTL = snksNTL, actTL = actTL, snksTL = snksTL)
   
   return(prepdat)
 
--- SCR_VIS_lurenolure.R
+++ SCR_VIS_lurenolure.R
@@ -9,6 +9,8 @@ rm(list = ls())
 library(dplyr)
 library(reshape2)
 library(tidyverse)
+library(secr)
+library(jagsUI)
 
 source("PrepData.R")
 
@@ -20,4 +22,119 @@ survs <- read.csv("Surveys.csv")[,c("EFFORTID","Date","TRANSECT","TYPE")]
 # Format for analysis
 dat <- PrepDat(caps,survs)
 
+# Create trapping grid of CP dimensions (5 ha, 50,000 m2)
+locs <- secr::make.grid(nx = 13, ny = 27, spacex = 16, spacey = 8)
+X <- as.matrix(locs)
+nolurepts <- X
+lurepts <- X[c(14:26,40:52,66:78,92:104,118:130,144:156,170:182,196:208,222:234,248:260,274:286,300:312,326:338),]
+
+## Define state-space of point process. (i.e., where animals live).
+## "delta" just adds a fixed buffer to the outer extent of the traps.
+## Don't need to estimate state-space since we know it (5 ha enclosed pop)
+delta<- 11.874929
+Xl<-min(locs[,1]) - delta
+Xu<-max(locs[,1]) + delta
+Yl<-min(locs[,2]) - delta
+Yu<-max(locs[,2]) + delta
+# Check area: 
+A <- (Xu-Xl)*(Yu-Yl)
+
+# Observations
+ynolure <- as.matrix(dat$snksNTL[,-1])
+colnames(ynolure) <- NULL
+ylure <- as.matrix(dat$snksTL[,-1])
+colnames(ylure) <- NULL
+
+## Number of locations (VIS lure/no lure points)
+JNL <- nrow(ynolure)
+JL <- nrow(ylure)
+
+# Active/not active for when transects run (one less day surveyed for TL)
+actNTL <- dat$actNTL[,-c(1,32)]
+colnames(actNTL) <- NULL
+actTL <- dat$actTL[,-c(1,31)]
+colnames(actTL) <- NULL
+
+# Uniquely marked individuals
+nindNL <- nrow(ynolure)
+nindL <- nrow(ylure)
+
+## Date augmentation
+M <- 250
+ynolure <-rbind(ynolure,array(0,dim=c((M-nrow(ynolure)),ncol(ynolure))))
+ylure <-rbind(ylure,array(0,dim=c((M-nrow(ylure)),ncol(ylure))))
+
+## Effort
+KNL <- rowSums(actNTL)
+KL <- rowSums(actTL)
+
+## Starting values for activity centers
+## set inits of AC to mean location of individuals and then to some area within stat space for augmented
+set.seed(552020)
+sst <- cbind(runif(M,Xl,Xu),runif(M,Yl,Yu))
+for(i in 1:nindNL){
+  sst[i,1] <- mean( X[ynolure[i,]>0,1] )
+  sst[i,2] <- mean( X[ynolure[i,]>0,2] )
+}
+
+
+## JAGS model
+cat(file="SCR0_DataAug.txt","
+model {
+  lam1 ~ dunif(0,5)  # Lure
+  lam2 ~ dunif(0,5)  # No lure
+  sigma ~ dunif(0,50)
+  psi ~ dunif(0,1)
+
+  for(i in 1:M){
+    z[i] ~ dbern(psi)
+    s[i,1] ~ dunif(Xl,Xu)
+    s[i,2] ~ dunif(Yl,Yu)
+    
+    ## NO LURE SURVEYS ##
+    for(t in 1:JNL) {
+      dNL[i,t]<- pow(s[i,1]-nolurepts[t,1],2) + pow(s[i,2]-nolurepts[t,2],2)
+      pNL[i,t]<- z[i]*lam2*exp(-(dNL[i,t])/(2*sigma*sigma))
+      ynolure[i,j] ~ dpois(pNL[i,j]*KNL)
+    }
+    
+    ## LURE SURVEYS ##
+    for(j in 1:JL){
+      dL[i,j]<- pow(pow(s[i,1]-lurepts[j,1],2) + pow(s[i,2]-lurepts[j,2],2),0.5)
+      pL[i,j]<- z[i]*lam1*exp(-(dL[i,j]*dL[i,j])/(2*sigma*sigma))
+      ylure[i,j] ~ dpois(pL[i,j]*KL)   
+    }
+  }
+  
+  N <- sum(z[])
+  D <- N/A
+  
+}")
+
+# MCMC settings
+nc <- 3; nAdapt=1000; nb <- 1; ni <- 2000+nb; nt <- 1
+
+# data and constants
+jags.data <- list (ynolure=ynolure, ylure=ylure, X=X, M=M, JNL=JNL, JL=JL, Xl=Xl, Xu=Xu, Yl=Yl, Yu=Yu, A=A, KNL=KNL, KL=KL)
+
+inits <- function(){
+  list (sigma=runif(1,1,50), z=c(rep(1,nindNL),rep(0,M-nindNL)), s=sst, psi=runif(1), lam1=runif(1,.5,1.5), lam1=runif(1,.5,1.5))
+}
+
+parameters <- c("sigma","psi","N","D","lam1","lam2","pNL","pL")
+
+out <- jagsUI("SCR0_DataAug.txt", data=jags.data, inits=inits, parallel=TRUE,
+            n.chains=nc, n.burnin=nb,n.adapt=nAdapt, n.iter=ni, parameters.to.save=parameters)
+
+save(out, file="SCRVISlurenolure.RData")
+
+print(out)
+traceplot(out$samples[,"lam1"])
+
+# gamma parameters for sigma
+a.sigma = (out$mean$sigma)^2/(out$sd$sigma)^2
+b.sigma = out$mean$sigma/(out$sd$sigma)^2
+N = out$mean$N
+
+plot(rgamma(N, a.sigma, b.sigma), type = "h", xlim=c(0,130))
 
