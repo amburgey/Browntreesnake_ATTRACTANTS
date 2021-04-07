023e84d61e6681546f651796b13ba6eec6c9e5cb
Staci Amburgey <staci.m.amburgey@gmail.com>
2021-04-07 09:24:23 -0700

    Comparison of iterations where lam0 estimated greater depending on treatment

---
+++ EncounterRateComparisons.R
@@ -0,0 +1,39 @@
+### Summarizing results
+
+library(jagsUI)
+
+## Probability that lamba[lure] > lambda[no lure]
+load("SCRVISlurenolure.RData")
+
+## Difference between lambda[lure] - lambda[no lure]
+temp <- out$sims.list$lam0[,2] - out$sims.list$lam0[,1]
+## Number of times where lamba[lure] was larger
+larger <- temp[temp > 0]
+## Proportion that lure was greater than no lure out of all iterations
+length(larger)/length(out$sims.list$lam0[,1])
+
+
+## Probability that lamba[no scent] > lambda[fresh scent], lambda[no scent] > lambda[old scent],lambda[old scent] > lambda[fresh scent]
+load("SCRVISscentnoscentGroupOwnCat.RData")
+
+## Difference between lambda[no scent] - lambda[fresh scent]
+temp <- out$sims.list$lam0[,1] - out$sims.list$lam0[,2]
+## Number of times where lamba[no scent] was larger
+larger <- temp[temp >= 0]
+## Proportion that no scent was greater than fresh scent out of all iterations
+length(larger)/length(out$sims.list$lam0[,1])
+
+## Difference between lambda[no scent] - lambda[old scent]
+temp <- out$sims.list$lam0[,1] - out$sims.list$lam0[,3]
+## Number of times where lamba[no scent] was larger
+larger <- temp[temp >= 0]
+## Proportion that no scent was greater than old scent out of all iterations
+length(larger)/length(out$sims.list$lam0[,1])
+
+## Difference between lambda[old scent] - lambda[fresh scent]
+temp <- out$sims.list$lam0[,3] - out$sims.list$lam0[,2]
+## Number of times where lamba[old scent] was larger
+larger <- temp[temp >= 0]
+## Proportion that old scent was greater than fresh scent out of all iterations
+length(larger)/length(out$sims.list$lam0[,1])
+
